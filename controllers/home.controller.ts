8bba4fcc7c9deb21c7e5a58b1f0db7f1934d69d9
Juan M. Medina <juanmanuelmedina@hotmail.com>
2023-10-20 09:57:22 -0500

    Added HOSTNAME to illustrate Load balancing

--- controllers/home.controller.ts
+++ controllers/home.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
+import os from 'os';
 
 export const hello = (req: Request, res: Response) => {
-	res.send('Hello Node');
+	const pod_name = process.env.HOSTNAME || os.hostname();
+	res.send(`Hello Node from ${pod_name}`);
 }
 
